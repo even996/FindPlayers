dbb70d4e6a6e318b03f4e5e0e0496004fdaee116
even996 <evenolsen96@gmail.com>
2020-04-12 17:41:30 +0200

    OnSkill layout created

--- app/src/main/java/com/eveno/findplayers/LeagueActiivty.kt
+++ app/src/main/java/com/eveno/findplayers/LeagueActiivty.kt
@@ -1,12 +1,20 @@
 package com.eveno.findplayers
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class LeagueActiivty : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league_actiivty)
+    }
+
+
+    fun leagueNextClicked(view: View){
+        val skill = Intent(this, SkillActivity::class.java)
+        startActivity(skill)
     }
 }

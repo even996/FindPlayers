66b7ecb538d2e882bae3012e29aca3bdc9752247
even996 <evenolsen96@gmail.com>
2020-04-12 20:51:39 +0200

    SavedInstacne saves the information when the phone turns.
    added icon using andoid icon studio

--- app/src/main/java/com/eveno/findplayers/Controller/LeagueActiivty.kt
+++ app/src/main/java/com/eveno/findplayers/Controller/LeagueActiivty.kt
@@ -13,9 +13,21 @@ class LeagueActiivty : BaseActivity() {
 
     var player = Player("","")
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState?.putParcelable(EXTRA_PLAYER, player)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league_actiivty)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if(savedInstanceState != null){
+            player = savedInstanceState.getParcelable(EXTRA_PLAYER)!!
+        }
     }
 
     fun onMensClicked(view: View){
--- app/src/main/java/com/eveno/findplayers/Controller/SkillActivity.kt
+++ app/src/main/java/com/eveno/findplayers/Controller/SkillActivity.kt
@@ -2,6 +2,7 @@ package com.eveno.findplayers.Controller
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.View
 import android.widget.Toast
 import com.eveno.findplayers.Model.Player
@@ -14,12 +15,22 @@ class SkillActivity : BaseActivity() {
     // later skal vi intitalizere det
     lateinit var player :Player
 
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        outState?.putParcelable(EXTRA_PLAYER, player)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill)
         player = intent.getParcelableExtra(EXTRA_PLAYER)
     }
 
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if(savedInstanceState != null){
+            player = savedInstanceState.getParcelable(EXTRA_PLAYER)!!        }
+    }
     fun onBallerClick(view: View) {
         beginnerSkillBtn.isChecked = false
         player.skill = "baller"
